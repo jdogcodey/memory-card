ddd9d3a838cd576dc94790008746c84c061d5023
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-09 19:54:24 +0100

    feat: Header title and github logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
+import data from './card-image.json';
+import Header from './Header';
 
 function App() {
+  
 
+  return (
+    <Header />
+
+  )
 }
 
 export default App
